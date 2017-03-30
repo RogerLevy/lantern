ee7ad522f5f146a1089c4f3f3557d4db134ce3b9
rogerlevy <roger.levy@gmail.com>
2017-03-29 21:02:38 -0400

    add compiler variables to portion.f

--- mo/portion.f
+++ mo/portion.f
@@ -1,10 +1,10 @@
 [bu] idiom [portion]
 import mo/cellstack
 
-#128 constant /portion
-2 megs constant /heap
+[undefined] /portion [if] 64 cells constant /portion [then]
+[undefined] /heap [if] 1 megs constant /heap [then]
 /heap /portion / constant #portions
-create heap /heap allot
+create heap /heap /allot
 #portions cellstack free-portions
 : reset-portions  free-portions vacate  heap  #portions 0 do  dup free-portions push  /portion +  loop  drop ;
 : portion  ( -- adr )  free-portions pop ;
