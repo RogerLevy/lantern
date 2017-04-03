d0bbc0756fbb3d0fb9cd2016991f81793feeb4e8
rogerlevy <roger.levy@gmail.com>
2017-04-03 02:52:37 -0400

    change behavior of `ld` and make `rld` related to it (simpler usage for both)

--- bu/dev/ld.f
+++ bu/dev/ld.f
@@ -1,13 +1,25 @@
 \ interactive testing tool LD
 
-create workdir 256 allot  " . " workdir place
 create lastld  256 allot
-: ld  ( -- <filespec> )  \ if filespec is nothing, loads last loaded file
-    bl parse  dup 0= if  2drop  lastld count  then
-    s[ ]s default.ext lastld place
-    workdir c@ if
-        workdir count  " \" strjoin  lastld count  strjoin  2dup file-exists if  included  exit then
-        2drop
+
+: fullpath ( adr c -- adr c )
+    tempbuf 0 locals| dummy outbuf |
+    zstring #255 outbuf &of dummy GetFullPathName GetLastError ?dup if zcount ?abort then
+    outbuf swap ;
+
+: ld  ( -- <filespec> )  \ if filespec is nothing, displays last loaded file
+    bl parse  dup 0= if
+        2drop  lastld count type exit
+    else
+        2dup  s[ ]s default.ext  fullpath lastld place
     then
-    lastld count  included  ;
+    included  ;
+
+: rld
+    cr ." Press Enter to reload the following file, or any other key to cancel:"
+    cr lastld count type
+    key #13 = -exit
+    " empty" evaluate  lastld count included ;
+
+create workdir 256 allot
 : make  workdir count " \make.bat" strjoin >shell ;
--- dev.f
+++ dev.f
@@ -1,18 +1,20 @@
 \ Development environment loader
 empty
 
-include bu/core/preamble         \ base dependencies, incl. Allegro, loaded once per session
-true constant dev
-gild
-
-s" envconfig.f" file-exists [if]
-    include envconfig.f
+defined dev 0<> nip not [if]
+    include bu/core/preamble         \ base dependencies, incl. Allegro, loaded once per session
+    include bu/dev/ld   including -name workdir place
+    true constant dev
+    s" envconfig.f" file-exists [if]
+        include envconfig.f
+    [then]
+    : /autoexec  " ld " s[  workdir count +s  " \autoexec" +s  ]s evaluate ;
     gild
 [then]
 
 [defined] NO_IDE [if]
     include bu/core/core.f ide
-    include autoexec.f
+    /autoexec
 [else]
-    include bu/dev/ide.f ide
+\    include bu/dev/ide.f ide
 [then]
