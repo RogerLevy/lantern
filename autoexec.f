64461cc7fb70b5e76d0440cc3bbbff4e10093de6
rogerlevy <roger.levy@gmail.com>
2017-05-02 00:28:44 -0400

    comment out project tool stuff...

--- autoexec.f
+++ autoexec.f
@@ -3,11 +3,11 @@ cr
 cr .( Directory: )
 cr
 
-: project
-    create cr  last @ count type  space space space  0 parse 2dup type
-            string,
-    does> count rld$ place  rld$ count included ;
-
-project dumbtest ex/dumbtest/main
-\ project race relayrace/main
-project arnie arnie/dev
+\ : project
+\     create cr  last @ count type  space space space  0 parse 2dup type
+\             string,
+\     does> count lastld place  lastld count included ;
+\
+\ project dumbtest ex/dumbtest/main
+\ \ project race relayrace/main
+\ project arnie arnie/dev
