89a762d23ad163ad0afa35f21bdceb58cdad7801
rogerlevy <roger.levy@gmail.com>
2017-05-09 02:53:32 -0400

    Fix bug with imports being added to the search order in reverse

--- bu/lib/idiom.f
+++ bu/lib/idiom.f
@@ -132,8 +132,8 @@ defer onSetIdiom  ' noop is onSetIdiom
 : wordlists+  ( idiom -- )
   'idiom @ >r  'idiom !
   @parent ?dup if  recurse  then  \ add parents' stuff first!
-  others> @+ ?dup if  cells bounds swap cell-
-                        do  i @ >publics +order  -cell +loop
+  others> @+ ?dup if  cells bounds
+                        do  i @ >publics +order  cell +loop
                   else  drop  then
   @publics +order
   r> 'idiom ! ;
