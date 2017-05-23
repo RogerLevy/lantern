472b8ba07145439ce138e4ac765fb2778523c7f8
rogerlevy <roger.levy@gmail.com>
2017-05-23 03:38:07 -0400

    rename hold to hold>

--- bu/core/gfx.f
+++ bu/core/gfx.f
@@ -18,9 +18,9 @@ transform m0
 : 1-1  m0 al_use_transform ;
 
 decimal
-: hold  1 al_hold_bitmap_drawing  r> call  0 al_hold_bitmap_drawing ;
-0 constant FLIP_NONE
-1 constant FLIP_H
-2 constant FLIP_V
-3 constant FLIP_HV
+    : hold>  ( -- <code> )  1 al_hold_bitmap_drawing  r> call  0 al_hold_bitmap_drawing ;
+    0 constant FLIP_NONE
+    1 constant FLIP_H
+    2 constant FLIP_V
+    3 constant FLIP_HV
 fixed
