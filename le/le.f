7e64929fadbff18299b3f7e3bf617aa7b18248fc
rogerlevy <roger.levy@gmail.com>
2017-06-06 00:39:42 -0400

    Lantern: Comment out GLSL and old "advanced" actor code

--- le/le.f
+++ le/le.f
@@ -6,7 +6,7 @@ bu: idiom le:
 \ import modules
 import mo/image
 import mo/draw
-import mo/glsl
+\ import mo/glsl
 import mo/gameutils
 import mo/portion
 import mo/rsort
@@ -19,13 +19,3 @@ include le/audio
 include le/autodata
 include le/kb
 include le/joy
-
-\ actor
-include le/actor0
-include le/actor1
-include le/actor2
-include le/actor3
-include le/actor4
-
-\ game modes
-\ include le/modes
