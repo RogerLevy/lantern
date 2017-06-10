0170f88d592495316541d5e3bc89bcd695ad8549
rogerlevy <roger.levy@gmail.com>
2017-06-10 13:21:28 -0400

    rename objtbl to obj and make some tweaks

--- le/obj.f
+++ le/obj.f
@@ -1,7 +1,7 @@
 \ Object Table (Strategy) for Bubble
 \ Adapted from Dark Blue / RetroForth
 \ For 2D games.
-\ This should thought of as a starting point.  It can be extended using idiom parenting.
+\ This file should thought of as a starting point.  We extend it in various files.
 \ When I have Workspaces to a certain level, editing source code in a visual way will take the place
 \  of the traditional level editor.  As a very preliminary example,
 \    Some object classes would NOT have an angle var.  Instead any rotation is hardcoded.
@@ -12,7 +12,7 @@
 \        rendering routines. You DON'T need this variable all the time.
 
 
-bu: idiom objtbl:
+bu: idiom obj:
     import mo/portion
     import mo/cellstack
     import mo/node
@@ -35,6 +35,8 @@ public:
 : ofs  create ,  does> @ me + ;
 : field  used @ ofs  swap used +! ;
 : var   cell field ;
+: 's    " me swap to me " evaluate  bl parse evaluate  " swap to me" evaluate ; immediate
+
 
 var en var nam var x var y var vx var vy var hide var disp var beha
 used @ value parms
@@ -45,22 +47,26 @@ container instance objects
 : each>  r> objects first @ begin  dup while  dup next @ >r  me!  dup >r  call  r>  r> repeat  2drop ;
 : /obj  heap dims drop ;
 : enable  me /obj erase  en on  hide on ;
-: named  ( -- <name> )  create me ,  nam on  does> @ me! ;
-\ : group  ( n )  o @ dup me! obj maxsize * o +! o @ cell- on ;
-\ : single  1 group enable ;
-: one  heap portion  me!  me objects pushnode  enable  at@ x 2! ;
+: init   me objects pushnode  enable  at@ x 2! ;
+: single  ( -- <name> )  create here me!  /obj allot  init  nam on ;
+: one    heap portion me!  init ;
 : draw>  r> disp !  hide off  ;
 : act>   r> beha ! ;
-: class  parms used ! ;
 : from   ( x y -- x y )  x 2@ 2+ ;
 : flash  #frames 2 and hide ! ;
 
 \ "external" words
 : place   ( x y obj -- )  { me!  x 2! } ;
-: as>     ( obj -- <code> )  r>  { me!  call } ;
 : delete  ( obj -- )  as>  me remove  nam @ not if  me heap recycle  then ;
 
 : end  me delete ;
+
+\ Private idioms for game objects
+: role  parms used !  obj: idiom ;
+
+
+
+\ Text
 
 [defined] dev [if]
     import mo/draw
