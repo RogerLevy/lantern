6435466d3d44ce6753008bb8f40bb9631bac5b38
rogerlevy <roger.levy@gmail.com>
2017-06-11 00:58:36 -0400

    bubble: tweaks to image module, added imgsubbmp

--- mo/image.f
+++ mo/image.f
@@ -3,7 +3,7 @@ bu: idiom image:
 \ ---------------------------------- images -----------------------------------
 0
   xvar bmp  xvar subw  xvar subh  xvar fsubw  xvar fsubh
-  xvar subcols  xvar subrows  xvar numSubimages
+  xvar subcols  xvar subrows  xvar subcount
 struct /image
 
 : initImage  ( ALLEGRO_BITMAP image -- ) bmp ! ;
@@ -20,11 +20,13 @@ struct /image
 : subdivide  ( tilew tileh image -- )
   >r  2dup r@ subw 2v!  2af r@ fsubw 2v!
   r@ imageDims r@ subw 2v@ 2/ 2pfloor  2dup r@ subcols 2v!
-  *  r> numSubimages ! ;
+  *  r> subcount ! ;
 
 : >subxy  ( n image -- x y )                                                    \ locate a subimage by index
-  >r  pfloor  r@ subcols @  /mod  2pfloor  r> subw 2v@ 2* ;
+    >r  pfloor  r@ subcols @  /mod  2pfloor  r> subw 2v@ 2* ;
 
 : afsubimg  ( n image -- ALLEGRO_BITMAP fx fy fw fh )                           \ helps with calling Allegro blit functions
-  >r  r@ bmp @  swap r@ >subxy 2af  r> fsubw 2v@ ;
+    >r  r@ bmp @  swap r@ >subxy 2af  r> fsubw 2v@ ;
 
+: imgsubbmp  ( n image -- subbmp )
+    >r  r@ bmp @  swap r@ >subxy  r> subw 2v@   4i  al_create_sub_bitmap ;
