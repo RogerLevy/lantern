845dd57f1dbda602d42c2122d690445ca9ab52ac
rogerlevy <roger.levy@gmail.com>
2017-06-14 11:08:06 -0400

    STOP now clears the given variable

--- le/audio.f
+++ le/audio.f
@@ -10,7 +10,7 @@ le:
     s @ r> al_set_audio_stream_playmode drop
     s @ mixer al_attach_audio_stream_to_mixer drop ;
 
-: stop  ( variable -- )  @ ?dup -exit  dup 0 al_set_audio_stream_playing drop  al_destroy_audio_stream ;
+: stop  ( variable -- )  @ ?dup -exit  dup 0 al_set_audio_stream_playing drop  dup al_destroy_audio_stream  off ;
 
 variable bgm
 : play  ( path c -- )  bgm stop  ALLEGRO_PLAYMODE_LOOP bgm stream ;
