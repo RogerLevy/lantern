cd44721f628bad2608ee66e0e935d9b0e31afc0d
rogerlevy <roger.levy@gmail.com>
2017-06-14 09:37:37 -0400

    rename fin to stop

--- le/audio.f
+++ le/audio.f
@@ -10,9 +10,9 @@ le:
     s @ r> al_set_audio_stream_playmode drop
     s @ mixer al_attach_audio_stream_to_mixer drop ;
 
-: fin  ( variable -- )  @ ?dup -exit  dup  al_set_audio_stream_playing drop  al_destroy_audio_stream ;
+: stop  ( variable -- )  @ ?dup -exit  dup 0 al_set_audio_stream_playing drop  al_destroy_audio_stream ;
 
 variable bgm
-: play  ( path c -- )  bgm fin  ALLEGRO_PLAYMODE_LOOP bgm stream ;
+: play  ( path c -- )  bgm stop  ALLEGRO_PLAYMODE_LOOP bgm stream ;
 
 : recite  ( path c variable -- variable )  dup >r   ALLEGRO_PLAYMODE_ONCE r@ stream  r> ;
