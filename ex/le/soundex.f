02dbe3032ae16f03390f3060dfdfc5576664fb2b
RogerLevy <roger.levy@gmail.com>
2017-07-25 21:34:33 -0400

    Cleanup soundex.f

--- ex/le/soundex.f
+++ ex/le/soundex.f
@@ -10,11 +10,10 @@ sfx *link* ex/le/link.wav
 
 : task  objects one ;
 
-: do-this  task 0 perform> 5 secs  bgm  0 ['] nextpart later  end ;
-: do-that  task 0 perform> begin  ['] *link* later0  1 secs  again ;
+: do-this  task 0 perform>  5 secs  ['] nextpart later0  end ;
+: do-that  task 0 perform>  begin  ['] *link* later0  1 secs  again ;
 
 " ex/le/asdf.ogg" play
-\ " ex/le/bgm006.it" play
 
 cr .( Streaming ogg audio for 5 seconds and playing a sound effect once a second...)
 do-this do-that
