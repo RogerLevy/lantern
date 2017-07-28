633807937bc30506887031721d8579fdd112956b
rogerlevy <roger.levy@gmail.com>
2017-07-28 17:41:53 -0400

    Add NPC wander logic in depthdemo.f

--- ex/le/depthdemo.f
+++ ex/le/depthdemo.f
@@ -1,4 +1,3 @@
-
 empty
 include le/le
 import bu/mo/rsort
@@ -8,20 +7,19 @@ autodata ex/le/ct
     18 20 alfador.image subdivide
     21 28 fairgirl.image subdivide
 
-0
-    enum DIR_DOWN
-    enum DIR_UP
-    enum DIR_RIGHT
-    enum DIR_LEFT
-drop
+\ ENUM doesn't work as expected...
+0 constant DIR_DOWN
+1 constant DIR_UP
+2 constant DIR_RIGHT
+3 constant DIR_LEFT
 
-var dir
-var img
-var data
+var dir    \ direction, see above enums
+var img    \ current image used for animation; tied to animation data (flipbooks)
+var data   \ static NPC data, like animation pointers
     \ internal:
-    var anm
-    var ctr
-    var spd  \ higher is slower
+    var anm  \ animation pointer
+    var ctr  \ animation counter
+    var spd  \ higher is slower; set by ANIMATE; note there is a hardcoded value passed by NPC-SPRITE
 
 : ?anmloop  anm @ @ 0 >= ?exit  anm @ cell+ @ anm ! ;
 : animate  ( speed -- adr | 0 )  \ adr points to a cell in the animation data
@@ -51,7 +49,10 @@ flipbook: cat-up     alfador.image  , 10 , 11 , 12 , 13 , ;
 : sprite  ( dir -- )   pfloor data@ execute ;
 : turn    ( dir -- )   pfloor dup dir !  sprite ;
 : flip@  dir @ flipdata@ ;
-: >feet  ( y -- y )  img @ subh @ - 4 + ;
+: chrw  img @ subw @ ;
+: sprh  img @ subh @ ;
+: chrh  16 ;
+: >feet  ( y -- y )  sprh - 4 + ;
 : npc-sprite  16 animate @ img @ afsubimg  at@ >feet 2af  flip@  al_draw_bitmap_region ;
 : npc   ( -- <name> )  ( -- me=newobj )    create
     does>  objects one  data !  DIR_DOWN turn  draw> npc-sprite ;
@@ -76,11 +77,41 @@ npc cat   ' cat-down , ' cat-up , ' cat-left , ' cat-left ,
 : enqueue  objects each>  hide @ ?exit  me , ;
 : draw-sorted  here dup  enqueue  compiled  2dup sort  drawem  reclaim ;
 
+\ Random walking logic; doing it without tasks to avoid unnecessary coupling
+\ quick n' dirty !!!
+var wc  \ walk counter, at 0 NPC will stop and after a bit turn in a random direction and walk.
+        \ if the NPC hits the boundaries it will just be impeded by a force field.
+
+defer wander
+defer walk
+: walkv  dir @ case
+        DIR_DOWN  of 0 0.5   exit endof
+        DIR_UP    of 0 -0.5  exit endof
+        DIR_RIGHT of 0.5 0   exit endof
+        DIR_LEFT  of -0.5 0  exit endof
+    endcase
+    0 0 ;
+: -wc  wc --  wc @ -1 > ;
+: ?turn  4 rnd turn ;
+: aboutface  dir @ 1 xor turn ;
+: limit
+    x @ 0 <  x @ displayw 3 / chrw - > or
+    y @ chrh < or  y @ displayh 3 / > or if  aboutface  walk  then
+    x 2v@  0 chrh  displaywh 3 3 2/  chrw 0 2- 2clamp  x 2v!
+    ;
+: :is  :noname  postpone [  [compile] is  ] ;
+:is walk   walkv vx 2v!
+    0.5 2 between fps * wc !
+    act>  limit  -wc ?exit  wander ;
+:is wander  0 0 vx 2v!
+    1 3 between fps * wc !
+    act>  limit  -wc ?exit  ?turn walk ;
+
 
 \ Create a bunch of randomized NPC's
 create roster  ' girl , ' man , ' cat ,
 : someone   3 rnd cells roster + @  execute ;
-: sprinkle  0 do  displaywh 3 3 2/ 2rnd at  someone  4 rnd turn  loop ;
+: sprinkle  0 do  displaywh 3 3 2/ 2rnd at  someone  ?turn  wander  loop ;
 
 \ 3X scaling
 transform m0  m0 3 3 2af  al_scale_transform
@@ -90,4 +121,4 @@ transform m0  m0 3 3 2af  al_scale_transform
 \ Do it
 ' 3x is prerender
 ' draw-sorted is render
-250 sprinkle
+250 sprinkle  me mem
