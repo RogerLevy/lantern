d97d7c4998d4dff1b50351959bb77a5e1370c006
rogerlevy <roger.levy@gmail.com>
2017-08-06 10:15:12 -0400

    Add files to tilemap testing

---
+++ ex/le/lk/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="20" tileheight="20" tilecount="8" columns="8">
+ <image source="tiles.png" trans="000000" width="160" height="20"/>
+</tileset>
