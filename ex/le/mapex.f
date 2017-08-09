4426d4edec25f102f502e5396f9564b91f6ec228
rogerlevy <roger.levy@gmail.com>
2017-08-09 14:28:34 -0400

    Progress; data seems to be getting loaded

--- ex/le/mapex.f
+++ ex/le/mapex.f
@@ -4,6 +4,6 @@ le: idiom mapex:
     import le/mo/loadtmx
 
 " ex/le/lk/test.tmx" opentmx
-\ load-tiles
-\ 0 layer[]  0 0 tilemap addr  2048 cells  extract   \ extract tilemap.  we assume layer 0 exists.
-\ scene  0 objgroup[]  load-objects
+load-tiles
+0 layer[]  0 0 tilemap addr  2048 cells  extract   \ extract tilemap.  we assume layer 0 exists.
+scene  0 objgroup[]  load-objects
