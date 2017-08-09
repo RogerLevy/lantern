930858bb5a1c747b2eee78f796e846b0467cb729
rogerlevy <roger.levy@gmail.com>
2017-08-09 17:40:44 -0400

    Mapex.f is now displaying a tilemap and a solitary tile

--- ex/le/mapex.f
+++ ex/le/mapex.f
@@ -1,9 +1,29 @@
+\ [ ] - A way to convert tiles on extraction
+
 empty
 include le/le
-le: idiom mapex:
-    import le/mo/loadtmx
+\ le: idiom mapex:
+import le/mo/loadtmx
 
 " ex/le/lk/test.tmx" opentmx
 load-tiles
-0 layer[]  0 0 tilemap addr  2048 cells  extract   \ extract tilemap.  we assume layer 0 exists.
+2048 2048 array2d tilebuf 
+0 layer[]  0 0 tilebuf addr  2048 cells  extract   \ extract tilemap.  we assume layer 0 exists.
+tilebuf convert-map
 scene  0 objgroup[]  load-objects
+
+var sx var sy
+
+: chest  draw>  2 tile blit ;
+
+: tilemap  draw>  0 0 320 240 scaled clip>  0 0 tilebuf addr  2048 cells  draw-tilemap ;
+
+\ 3X scaling
+transform m0  m0 3 3 2af  al_scale_transform
+: magnified  grey backdrop  m0 al_use_transform  ;
+
+scene
+' magnified is prerender
+50 50 at one chest
+0 0 at one tilemap named mytm
+
