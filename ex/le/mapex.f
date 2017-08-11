1700c636419989a33e0c8e1f0bebde076082f6e8
rogerlevy <roger.levy@gmail.com>
2017-08-11 13:27:15 -0400

    Factor out tilemap loading and display into tilemap.f

--- ex/le/mapex.f
+++ ex/le/mapex.f
@@ -2,33 +2,15 @@
 
 empty
 include le/le
-import le/mo/loadtmx
+import le/mo/tilemap
 
 soft  320 240 *bmp value framebuf
-2048 2048 array2d tilebuf
+map testmap ex/le/lk/test.tmx
 
-
-" ex/le/lk/test.tmx" opentmx
-crisp  load-tiles
-    0 layer[]  0 0 tilebuf addr-pitch  extract   \ extract tilemap.  we assume layer 0 exists.
-    0 0 1024 1024 tilebuf convert-tilemap
-    scene  0 objgroup[]  load-objects
-
-var sx var sy
+crisp testmap open  0 layer[] 0 0 read
+scene  0 objgroup[]  load-objects
 
 : chest  draw>  2 tile 3 ublit ;
-
-: tilemap
-    act>
-        <up> kstate if -1 sy +! then
-        <down> kstate if  1 sy +! then
-        <left> kstate if -1 sx +! then
-        <right> kstate if  1 sx +! then
-        sx 2v@ 0 0 2max sx 2v!
-    draw>
-        0 0 at
-        at@ 320 240 scaled clip>
-        sx 2v@ 20 20 scroll tilebuf addr  tilebuf @pitch  draw-tilemap-bg ;
 
 : pre-mag   grey backdrop  framebuf onto  black backdrop ;
 : mag       framebuf onto  draw-world ;
@@ -40,5 +22,15 @@ scene
 ' post-mag is postrender
 
 50 50 at one chest
+
 0 0 at one tilemap named mytm
+    tilemap: 320 240 mw 2v!
+    : scroll
+        act>
+        <up> kstate if -1 sy +! then
+        <down> kstate if  1 sy +! then
+        <left> kstate if -1 sx +! then
+        <right> kstate if  1 sx +! then
+        sx 2v@ 0 0 2max sx 2v! ; scroll
+
 
