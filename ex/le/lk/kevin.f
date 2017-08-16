5bb8ef081fa7aa4b6b0968af2f04694aa06da07e
rogerlevy <roger.levy@gmail.com>
2017-08-16 02:08:47 -0400

    Cleanup things a bit

--- ex/le/lk/kevin.f
+++ ex/le/lk/kevin.f
@@ -3,9 +3,9 @@ role kevin:
     import le/oe/tiled
 
     \ mixins
-    include le/om/flipbook
+    include le/om/flipbook   \ adds animation features to this object
 
-    image kevin.image ex/le/lk/chr_lk.png
+    crisp image kevin.image ex/le/lk/chr_lk.png
     20 20 kevin.image subdivide
 
     \ typical stuff
@@ -46,29 +46,22 @@ role kevin:
     : can-jump   <jump> kpressed if jump then ;
     : can-walk   <left> kstate if walkL then   <right> kstate if walkR then ;
     : can-squat   <squat> kstate if squat then ;
+    : can-die ;
 
     : control-in-air
        <left> kstate if  -0.2 vx +!  faceleft then
        <right> kstate if  0.2 vx +!  faceright then
        vx @ 0.95 *  walkspeed @ vx @ 0< if negate max else min then vx ! ;
-
-    : can-die
-    \   <enter> kstate if die then
-    ; \    stamina @ 0 <= if die then ;
-
     : drift   vx @ 0.90 * vx !   ;
     : movelegs   ( n. - )  abs 5.0 swap -   anmspd ! ;
     : ?walk.anm   @playing walk.anm <> if  walk.anm 1 animate  then ;
-
     : to-a-stop
         drift
         vx @ abs 0.2 <
             if   0 vx !   idle.anm 0 animate
             else  ?walk.anm  vx @ movelegs  then ;
-
     : walkAccelR   vx @ 0.14 + walkSpeed @ min dup vx ! movelegs ;
     : walkAccelL   vx @ 0.14 - walkSpeed @ negate max dup vx ! movelegs ;
-
     : ctrl  idle.anm 1 animate  act>  0 0 vx 2v!  2 vx udlr ;
     : draw-kevin  @anm+ img @ afsubimg  at@ 2af  flip@  al_draw_bitmap_region ;
     : animthru   animate  begin  pause  end-of-anm? until ;
@@ -99,14 +92,15 @@ role kevin:
     : hitmap  floor? not if  (fall)  else  in-air? if  (land)  then  then ;
     :is fall  (fall)  0 perform>  jump.anm 1 animate   begin  control-in-air   pause  again ;
     :is land  idle ;
-
+    : gravity  0.15 vy +! ;
     : conf
         3 1 mbx 2v!
         14 19 mbw 2v!
         1.6 walkspeed !
         2.75 jumppower !
         0.1022 risepower !
-        act>  0.15 vy +!  onhitmap> drop  hitmap ;
+        act>  gravity
+        onhitmap>  drop  hitmap ;
 
 public:
 : kevin  ( ctrl )  fall  conf  draw>  draw-kevin ;
--- ex/le/platformex.f
+++ ex/le/platformex.f
@@ -3,35 +3,32 @@
 
 empty
 include le/le
-include le/oe/tiled
+include le/oe/tiled         \ extend lantern with Tiled support
 
 le: idiom platformex:
-    import ex/le/lk/kevin
-    import le/mo/tilcd
-    
-\ variables
+    import ex/le/lk/kevin   \ character script
+
+\  Variables
 soft  320 240 *bmp value framebuf
 map testmap ex/le/lk/test.tmx
 objlist bg1
 
-
 \  Scene setup
 devoid scene
-crisp testmap open  0 layer[] 0 0 get
-bg1 in  0 0 at one tilemap named mytm  tilemap: 320 240 mw 2v!  platformex:
-objects in  20 20 at  one kevin named p1
+    crisp testmap open  0 layer[] 0 0 get
+    bg1 in  0 0 at one tilemap named mytm  tilemap: 320 240 mw 2v!  platformex:
+    objects in  20 20 at  one kevin named p1
 
+\  Scrolling
 : scrollxy  p1 's x 2v@   160 120 2-  0 0 2max ;
 : movecam  scrollxy mytm scroll-tilemap ;
 transform m0
 : scroll>  m0 al_identity_transform  m0 scrollxy 2negate 2af al_translate_transform  m0 al_use_transform  r> call  1-1 ;
 
-
+\  Main loop configuration; COLLIDE-OBJECTS-MAP enables tilemap collisions in conjunction with HITMAP> (see kevin.f)
 :noname [ is prerender ]  grey backdrop  framebuf onto  black backdrop ;
 :noname [ is render ]     movecam        framebuf onto  1-1 bg1 draw-objlist  scroll>  objects draw-objlist ;
 :noname [ is postrender ] displayw 320 3 * - 0 at  white  framebuf dup bmpwh 3 3 2* sblit ;
+:noname [ is poststep ]   eachlist> 20 collide-objects-map ;
 
-
-:noname [ is poststep ] eachlist> 20 collide-objects-map ;
-
-interact off
+interact off  \ start without IDE visible
